bcff6b0f3f0f512f45bdedb0c7ec91aaa6647dc6
Kimberly Rouse <ksrouse94@gmail.com>
2019-02-23 23:33:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,8 +34,6 @@ def find_the_cheese(array)# code an argument here
   array.each do |item|
     if cheese_types.include?(item)
       return item 
-    else 
-      return nil 
     end  
   end  
 end
